da92414474d1cd4e553e9f74380933946d7d5d44
James Suescun <james.steven2318@gmail.com>
2022-04-06 21:55:15 -0400

    crerate components & install dependencies

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+app.use(express.json()); // we can send json to our express server to put in our req.body
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+app.listen(PORT, () => {
+    console.log("on" + PORT);
+});

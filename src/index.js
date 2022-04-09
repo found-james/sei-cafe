d99e54e537daf0baebd7f916a842909a2bf3181b
James Suescun <james.steven2318@gmail.com>
2022-04-09 11:46:12 -0400

    update folder structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/App/App.js';
+import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/App/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NewOrderPage from '../NewOrderPage/NewOrderPage';
+import AuthPage from '../AuthPage/AuthPage';
+import OrderHistoryPage from '../OrderHistory/OrderHistoryPage';
+
+function App() {
+  const [user, setUser] = useState(null);
+  return (
+    <main className="App">
+    {
+      user ? 
+      <Routes>
+        <Route path="order/new" element={ <NewOrderPage /> } />
+        <Route path="order" element={ <OrderHistoryPage /> } />
+      </Routes>
+      : <AuthPage />
+    }
+    </main>
+  );
+}
+
+export default App;

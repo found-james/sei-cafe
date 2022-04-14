877ed880572d4c1e92ac3c3c4d10bc7549eb41fb
James Suescun <james.steven2318@gmail.com>
2022-04-13 22:01:34 -0400

    complete signup form and style form

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -10,8 +10,13 @@ class SignupForm extends Component {
     }
 
     handleChange = e => {
-        this.setState({...this.state, [e.tagert.name]: e.target.value});
-        
+        this.setState({...this.state, [e.target.name]: e.target.value, error: ''});
+
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        alert(JSON.stringify(this.state));
     }
     render (){
         const disable = this.state.password !== this.state.confirm;
@@ -26,12 +31,16 @@ class SignupForm extends Component {
                         <input type="text" name="name"
                         value={this.state.name} 
                         onChange={ this.handleChange} required />
+                        <label>email</label>
+                        <input type="email" name="email"
+                        value={this.state.email} 
+                        onChange={ this.handleChange} required />
                         <label>password</label>
-                        <input type="text" name="password"
+                        <input type="password" name="password"
                         value={this.state.password} 
                         onChange={ this.handleChange} required />
                         <label>confirm</label>
-                        <input type="text" name="confirm"
+                        <input type="password" name="confirm"
                         value={this.state.confirm} 
                         onChange={ this.handleChange} required />
                         <button type="submit" disabled={disable}>SIGN UP</button>                                    </form>

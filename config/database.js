b0d17c5803e23946d6470861b6745bea8e1ca70f
James Suescun <james.steven2318@gmail.com>
2022-04-13 20:12:18 -0400

    establish connection with db

---
+++ config/database.js
@@ -0,0 +1,9 @@
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+
+db.on('open', ()=> console.log('word'));
+
+module.exports = mongoose;
---
+++ crud-helper.js
@@ -0,0 +1,9 @@
+
+require('dotenv').config(); // brings in env variables from .env
+require('./config/database.js'); //bring in database connection
+
+// Require the Mongoose models
+// const User = require('./models/user');
+// const Item = require('./models/item');
+// const Category = require('./models/category');
+// const Order = require('./models/order');
